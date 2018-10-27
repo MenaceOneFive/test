9a236c87d1fc00ec9e9c369bb069ad18f910a75a
vsk12300 <l2395@naver.com>
2018-10-28 00:22:02 +0900

    Fourth Commit

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
     public static void main(String[] args){
     System.out.println("Hello git");
     System.out.println("Changes after First commit")
-
+    System.out.println("리모트 푸시후 변화!");
 
 }
 
